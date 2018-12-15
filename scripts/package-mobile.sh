c097ddd2b9f84203235c71ae91e31e0111dd64f7
mengxk <xiaokun.meng@qcast.cn>
2018-12-15 16:08:20 +0800

    add package-mobile

--- scripts/package-mobile.sh
+++ scripts/package-mobile.sh
@@ -6,16 +6,12 @@ set -o nounset
 CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
 PROJECT_DIR=$(dirname "$CURRENT_DIR")
 DEPENDS_DIR="$PROJECT_DIR/config/scripts";
+BUILD_SH="$CURRENT_DIR/build.sh";
 
-if [ ! -d "$DEPENDS_DIR" ]; then
-	git submodule init;
-	git submodule update;
-fi
-
-build_extfunc_depends()
+build_extfunc()
 {
-	"$DEPENDS_DIR/build-openssl.sh" $@;
+	"$BUILD_SH" $@;
 }
 export CFG_PROJECT_DIR="$PROJECT_DIR";
-source "$DEPENDS_DIR/build.sh" $@;
-
+export CFG_PROJECT_NAME="Elastos.ORG.Wallet.Lib.C";
+source "$DEPENDS_DIR/package-mobile.sh" $@;
