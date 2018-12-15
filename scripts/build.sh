c18c9c753ae1ca0998d65787604cf957a7ab5dd3
mengxk <xiaokun.meng@qcast.cn>
2018-12-15 15:12:41 +0800

    add pkg-config.in

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,17 +5,17 @@ set -o nounset
 
 CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
 PROJECT_DIR=$(dirname "$CURRENT_DIR")
-SCRIPT_DIR="$PROJECT_DIR/config/scripts";
+DEPENDS_DIR="$PROJECT_DIR/config/scripts";
 
-if [ ! -d "$SCRIPT_DIR" ]; then
+if [ ! -d "$DEPENDS_DIR" ]; then
 	git submodule init;
 	git submodule update;
 fi
 
 build_extfunc_depends()
 {
-	"$SCRIPT_DIR/build-openssl.sh" $@;
+	"$DEPENDS_DIR/build-openssl.sh" $@;
 }
 CFG_PROJECT_DIR="$PROJECT_DIR";
-source "$SCRIPT_DIR/build.sh" $@;
+source "$DEPENDS_DIR/build.sh" $@;
 
