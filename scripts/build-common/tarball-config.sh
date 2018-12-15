e64888291083a17be1ef7a4b9a3912423cffa423
mengxk <xiaokun.meng@qcast.cn>
2018-12-15 12:39:56 +0800

    move scripts to submodule.

--- scripts/build-common/tarball-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-
-OPENSSL_BASE_URL="https://www.openssl.org/source";
-OPENSSL_VERSION="1.1.1a";
-OPENSSL_NAME="openssl-$OPENSSL_VERSION";
-OPENSSL_TARBALL="$OPENSSL_NAME.tar.gz";
-
