3985d1a9a062949d124edd89558a3010d2a0c7e2
klx99 <xiaokun.meng@qcast.cn>
2020-09-27 20:30:14 +0800

    add filecoin build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,13 +11,40 @@ cd "$PROJECT_DIR";
 git submodule init;
 git submodule update;
 
+custom_getopt_usage()
+{
+	echo "
+           --with-filecoin
+                 Optional. build with filecoin library.";
+}
+custom_getopt_options()
+{
+	echo ";with-filecoin";
+}
+custom_getopt_processor()
+{
+	custom_getopt_processor_ret=-1;
+	case "$1" in
+		(     --with-filecoin)
+			CFG_WITH_FILECOIN=true;
+			custom_getopt_processor_ret=1;
+			;;
+	esac
+}
+
 build_extfunc_depends()
 {
 	"$DEPENDS_DIR/scripts/build-openssl.sh" $@;
+
+	logdbg "CFG_WITH_FILECOIN=$CFG_WITH_FILECOIN";
+	if [[ $CFG_WITH_FILECOIN == true ]]; then
+	    "$DEPENDS_DIR/scripts/build-filecoin-ffi.sh" $@;
+	fi
 }
 
 export CFG_PROJECT_NAME="Elastos.SDK.Keypair.C";
 export CFG_PROJECT_DIR="$PROJECT_DIR";
 export CFG_CMAKELIST_DIR="$PROJECT_DIR";
+CFG_WITH_FILECOIN=false;
 source "$DEPENDS_DIR/scripts/build.sh" $@ --force-build;
 
