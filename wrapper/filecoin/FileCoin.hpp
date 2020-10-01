c44fb8fbaa15a0264331e543e46b7ecc2fd46762
klx99 <xiaokun.meng@qcast.cn>
2020-10-01 17:49:31 +0800

    update FileCoin.hpp comments.

--- wrapper/filecoin/FileCoin.hpp
+++ wrapper/filecoin/FileCoin.hpp
@@ -18,6 +18,45 @@ bool Verify(const char* publicKey,
             const void* data, int len,
             const void* signedData, int signedLen);
 
+/**
+ * \~English
+ * Generate raw transaction data, sign transaction and serialize.
+ *
+ * @param
+ *      privateKey    [in] the private key.
+ *      transaction   [in] the transaction data in json string.
+ *                  ex. {
+ *                        "to": "t3xcnpgqifiwjivr65ylnxrvk3qjxb2hu5wz5b26z6kzr7z5shu4bicfwhv5vyoxyfiy6pjpj44cwndtmwe4ka",
+ *                        "from": "t3s7px2ud2iajvsuxnynq4dvf4wbxrey4ipt3csk444irrgsorq5ctrknrtqml5kwewifbgqikecgdgnmbpq5a",
+ *                        "value": "1",
+ *                        "gasPremium": "1000000",
+ *                        "gasFeeCap": "1000000",
+ *                        "gasLimit": 80000000,
+ *                        "method": 0,
+ *                        "nonce": 0,
+ *                        "params": ""
+ *                      }
+ * @return
+ *      the raw transaction data if succeeded, or nullptr if failed.
+ *      if you no longer use, call freeBuf to free memory.
+ *      ex.{
+ *           "Message": {
+ *             "to": "t3xcnpgqifiwjivr65ylnxrvk3qjxb2hu5wz5b26z6kzr7z5shu4bicfwhv5vyoxyfiy6pjpj44cwndtmwe4ka",
+ *             "from": "t3s7px2ud2iajvsuxnynq4dvf4wbxrey4ipt3csk444irrgsorq5ctrknrtqml5kwewifbgqikecgdgnmbpq5a",
+ *             "value": "1",
+ *             "gasPremium": "1000000",
+ *             "gasFeeCap": "1000000",
+ *             "gasLimit": 80000000,
+ *             "method": 0,
+ *             "nonce": 0,
+ *             "params": ""
+ *           },
+ *	         "Signature": {
+ *             "Type": 2,
+ *             "Data": "tkDHJTSQed2Iwzl5k6UrvB3A/bSXUDywcsmPzHfCvy7cI7u5/yLl3IEyfE9FKP9tA9+B5v0mSalgFkd1iR7JTKmrus9XIIPreuJIBnmtvxA+KSBGrxrBEjAYGzeIMeo8"
+ *	       }
+}
+ */
 char* GenerateRawTransaction(const char* privateKey, const char* transaction);
 
 } // namespace FileCoin
